0c4a48871a32b745af8f57853fbc3b7b345f3e4f
Jorge Jimenez <jjimenez17@alumnos.utalca.cl>
2024-05-08 20:17:06 -0400

    Primer commit -Shipus owo

---
+++ Ledotron/app/src/main/java/com/examples/ledotron/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.examples.ledotron
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //accion de boton para abrir otra ventana
+
+        val launchButton = findViewById<Button>(R.id.launchButton)
+        launchButton.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
